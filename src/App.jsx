bdf712d52588c344668be58e8d664379dfdfa60d
shaya-141 <133588655+shaya-141@users.noreply.github.com>
2025-01-24 16:05:24 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './page/login'
+import Signup from './page/signup'
+import Home from './page/home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+      </Routes>
+     
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/page/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <h1 className='text-[20px] text-fuchsia-400'>home</h1>
+  )
+}
+
+export default Home
